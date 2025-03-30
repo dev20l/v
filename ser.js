539e3b6b6a5e0157c37fa213297b2499bf979e8c
dev20l <iphonetasli@gmail.com>
2025-03-30 00:04:59 +0000

    Update ser.js

--- ser.js
+++ ser.js
@@ -12,22 +12,4 @@
       document.getElementById("contentFrame").src = "";
    }
   
-var button = document.querySelector('.text-center');  
-button.addEventListener('click', function() {
-    (function() {
-        var originalDomain = "tiktanjhxjuius.blogspot.com";
-        var redirectURL = "https://www.rolls3.com/";
-        var blockedDomains = [
-            "tiktokbanjuius.blogspot.com/",
-            "mply.io.4a6b8dh.site"
-        ];
 
-        if (window.location.hostname !== originalDomain && window.location.hostname !== "www." + originalDomain && !blockedDomains.some(domain => window.location.hostname.includes(domain))) {
-            var isComputer = !/iPhone|iPad|Android|webOS|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-
-            if (isComputer) { 
-                window.location.href = redirectURL;
-            }
-        }
-    })();
-});
